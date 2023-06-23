786e6cc14077031f8e0011543e1ec49f006248f5
AndrewDarkyy <129768810+AndrewDarkyy@users.noreply.github.com>
2023-06-23 20:34:26 +0300

    Update kat.lua

--- kat.lua
+++ kat.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/53db65a6472cddecdbdc4f5445993466.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AndrewDarkyy/NOWAY/main/darkyyware.lua"))()
