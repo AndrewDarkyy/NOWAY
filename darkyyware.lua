4ea52e30336cee4f85d5ec2d4595f9eac26efe54
AndrewDarkyy <129768810+AndrewDarkyy@users.noreply.github.com>
2023-06-28 12:50:10 +0300

    Update darkyyware.lua

--- darkyyware.lua
+++ darkyyware.lua
@@ -1,5 +1,6 @@
+-- check if executor supports darkyyware
 local httpget = request or http_request or (http and http.request) or (syn and syn.request)
-if httpget and loadstring and getrawmetatable and setreadonly and getnamecallmethod and coroutine and coroutine.wrap and Drawing and hookfunction and getgenv and gethui and sethiddenproperty and getconnections and getupvalue and getsenv and pcall then
+if httpget and hookmetamethod and getnamecallmethod and Drawing and hookfunction and getconnections and getupvalue and getsenv and setfpscap then
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/53db65a6472cddecdbdc4f5445993466.lua"))()
 else
     local Converted = {
@@ -8,10 +9,8 @@ else
         ["UICorner"] = Instance.new("UICorner");
         ["TextLabel"] = Instance.new("TextLabel");
     }
-
     Converted["ScreenGui"].ZIndexBehavior = Enum.ZIndexBehavior.Sibling
     Converted["ScreenGui"].Parent = game.CoreGui
-
     Converted["Frame"].BackgroundColor3 = Color3.fromRGB(30, 30, 30)
     Converted["Frame"].BorderColor3 = Color3.fromRGB()
     Converted["Frame"].BorderSizePixel = 0
@@ -19,11 +18,9 @@ else
     Converted["Frame"].Position = UDim2.new(0.367591411, 0, 0.324189514, 0)
     Converted["Frame"].Size = UDim2.new(0, 420, 0, 281)
     Converted["Frame"].Parent = Converted["ScreenGui"]
-
     Converted["UICorner"].Parent = Converted["Frame"]
-
     Converted["TextLabel"].Font = Enum.Font.FredokaOne
-    Converted["TextLabel"].Text = "Your executor does not support darkyyware"
+    Converted["TextLabel"].Text = "Your executor does not support darkyyware :("
     Converted["TextLabel"].TextColor3 = Color3.fromRGB(200, 200, 200)
     Converted["TextLabel"].TextSize = 50
     Converted["TextLabel"].TextWrapped = true
@@ -34,16 +31,11 @@ else
     Converted["TextLabel"].BorderSizePixel = 0
     Converted["TextLabel"].Size = UDim2.new(0, 420, 0, 285)
     Converted["TextLabel"].Parent = Converted["Frame"]
-
     game:GetService("TweenService"):Create(Converted["Frame"], TweenInfo.new(1), {BackgroundTransparency = 0}):Play()
     game:GetService("TweenService"):Create(Converted["TextLabel"], TweenInfo.new(1), {TextTransparency = 0}):Play()
-
     wait(4)
-
     game:GetService("TweenService"):Create(Converted["Frame"], TweenInfo.new(1), {BackgroundTransparency = 1}):Play()
     game:GetService("TweenService"):Create(Converted["TextLabel"], TweenInfo.new(1), {TextTransparency = 1}):Play()
-
     wait(2)
-
     Converted["ScreenGui"]:Destroy()
 end
